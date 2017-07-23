dad114c5ab16b9b79140e6d0a8f1c10c15da339f
Brian Faust <hello@brianfaust.de>
2017-07-23 05:52:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Client.php
+++ src/Client.php
@@ -33,7 +33,7 @@ class Client
      */
     public function __construct(string $key, string $subdomain)
     {
-        $this->key       = $key;
+        $this->key = $key;
         $this->subdomain = $subdomain;
     }
 
